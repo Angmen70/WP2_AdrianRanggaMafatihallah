77832907e08f7db5566150e640fcc59fb9ec37f5
Angmen70 <gendilaja70@gmail.com>
2024-03-28 18:06:11 +0700

    contoh1.php,latihan1.php,Model_latihan1.php,view_latihan1.php

---
+++ app/Controllers/contoh1.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class contoh1 extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+    public function coba()
+    {
+        echo "<h1>Perkenalkan</h1>" ;
+        echo "Nama saya Adrian Rangga" ;
+    }
+}
